b0edc69f7e172f235c2daf85286992b4b5702a69
Kellen1717 <kellenbwilliams@icloud.com>
2020-07-06 18:39:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello, welcome to Kellen's doughnuts")
+customerdoughnutsorder=input ("what kind of doughnuts do you want")
+howmany=input (" doughnuts do you want? ")
+print("Thank You for your order of " +customerdoughnutsorder)
+
