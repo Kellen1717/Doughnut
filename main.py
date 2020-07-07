5c324846ce03fba5ee70e0bdbda47103591a8732
Kellen1717 <kellenbwilliams@icloud.com>
2020-07-07 16:30:40 +0000

    stuff

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 print("Hello, welcome to Kellen's doughnuts")
-print ("what kind of doughnut do you want")
-myorder=input ("A - chocolate B - glazed C - strawbarry")
-toppings=""
-order=""
+:
+  print ("what kind of doughnut do you want")
+  myorder=input ("A - chocolate B - glazed C - strawbarry")
+  toppings=""
+  order=""
 
-if(myorder == "A" ):
-  toppings=input ("would you like sprikles")
-  order="chocolate"
-elif(myorder == "B"):
-  toppings=input ("would you like id warm")
-  order="glazed"
-elif(myorder == "C"): 
-  toppings=input ("would you like extra strawbarrys")
-  order="strawbarry"
-
+  if(myorder == "A" ):
+    toppings=input ("would you like sprikles")
+    order="chocolate doughnut"
+  elif(myorder == "B"):
+    toppings=input ("would you like it warm")
+    order="glazed doughnuts"
+  elif(myorder == "C"): 
+    toppings=input ("would you like extra strawbarrys")
+    order="strawbarry doughnuts"
+  print (" thank you for your order of a " +order +toppings)
 
 
 
